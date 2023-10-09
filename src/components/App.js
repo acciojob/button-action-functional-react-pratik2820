d55c8eb2363e83175461c7ab7ba09518ee857e64
pratik2820 <pratikmorge28@gmail.com>
2023-10-09 19:21:15 +0530

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,26 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  const[para,setPara] = useState(false);
+
+  function changePara(){
+    setPara(true)
+  }
+
+
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={changePara}>Click</button>
+
+      {
+        para && (
+          <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+        )
+      }
+      
+
     </div>
   );
 }
